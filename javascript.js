4cf87a1dd5b180bcf979647101ddcd7d0bc48ac7
Chris <173185832+odinca@users.noreply.github.com>
2024-07-23 11:44:29 +0100

    Write functions to build grid elements

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
-// Function to create "div" elements
+// Function to create element with ID
 
-
+function buildElement(elementName, elementID) {
+    const newElement = document.createElement(elementName);
+    newElement.setAttribute("id", elementID);
+    return newElement;
+}
 
 // Function to build grid
 
+function createGrid(){
+    let rowNum = 1;
+    let rowIDs = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"];
+    for (rowNum; rowNum <= 16; rowNum++) {
+        buildElement("div", "row" + rowNum.toString())
+    }
+}
