0192844974798112f13fdcc2028b534a707b1770
Chris <173185832+odinca@users.noreply.github.com>
2024-07-24 21:19:38 +0100

    Create function to add hover effect

--- javascript.js
+++ javascript.js
@@ -10,16 +10,29 @@ function buildElement(elementName, elementID, className) {
 // Function to build grid
 
 function createGrid(){
-    container = document.querySelector("#container");
-    let rowNum = 1;
+    const container = document.querySelector("#container");
     let columnIDs = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"];
-    for (rowNum; rowNum <= 16; rowNum++) {
+    for (let rowNum = 1; rowNum <= 16; rowNum++) {
         container.appendChild(buildElement("div", "row" + rowNum.toString(), "row"))
-        currentRow = document.querySelector(`#row${rowNum.toString()}`);
+        const currentRow = document.querySelector(`#row${rowNum.toString()}`);
         for (let columnID of columnIDs) {
             currentRow.appendChild(buildElement("div", rowNum + columnID, "gridItem"));
         }
     }
+    changeColour();
+}
+
+// Function to change colour of mouse selected element
+
+function changeColour() {
+    const square = document.querySelector("#container");
+
+    square.addEventListener("mouseover", (event) => {
+        if (event.target.classList == "gridItem") {
+        event.target.style.backgroundColor = "rgb(105 105 105)";
+        }
+    });
 }
 
 createGrid ();
+
