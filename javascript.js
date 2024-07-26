8aae114ac01b04a46810e6e8e73fc35c89e1229f
Chris <173185832+odinca@users.noreply.github.com>
2024-07-26 16:38:07 +0100

    Call function in main game

--- javascript.js
+++ javascript.js
@@ -14,6 +14,7 @@ function createGrid(squaresPerRow = 16){
             currentRow.appendChild(buildElement("div", "gridItem"));
         }
     }
+    squareDimensions(960, squaresPerRow);
     changeColour();   
 }
 
